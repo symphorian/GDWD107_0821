a7ce4399eddc3909c6e8ede7b2f8ed830a735f5e
symphorian <avaricSymphorian@gmail.com>
2022-04-12 13:03:34 -0400

    slideshow and ball

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,31 +1,41 @@
 let currentSlideIndex = 0;
-showSlide(currentSlideIndex);
 
-function showNextSlide() {
-  currentSlideIndex += 1;
-  showSlide(currentSlideIndex);
-}
-
-function showPreviousSlide() {
-  currentSlideIndex -= 1;
-  showSlide(currentSlideIndex);
-}
-
-function showSlide(index) {
+function showSlide() {
   debugger;
-  let slides = document.getElementsByClassName("slideshowImage");
+  let slide1 = document.getElementById('slide1');
+  let slide2 = document.getElementById('slide2');
+  let slide3 = document.getElementById('slide3');
+  let slide4 = document.getElementById('slide4');
+
+  // this creates an array of <img> elements
+  let slides = [slide1, slide2, slide3, slide4];
 
   if (currentSlideIndex >= slides.length) {
     currentSlideIndex = 0;
   }
+
   if (currentSlideIndex < 0) {
     currentSlideIndex = slides.length - 1;
   }
-  for(let i = 0; i < slides.length; i++) {
+
+  for (let i = 0; i < slides.length; i++) {
+    let loopSlide = slides[i];
     if (i != currentSlideIndex) {
-      slides[i].style.display = 'none';
+      loopSlide.style.display = 'none';
     } else {
-      slides[i].style.display = 'block';
+      loopSlide.style.display = 'block';
     }
   }
 }
+
+function showNextSlide() {
+  currentSlideIndex += 1;
+  showSlide();
+}
+
+function showPreviousSlide() {
+  currentSlideIndex -= 1;
+  showSlide();
+}
+
+showSlide();
